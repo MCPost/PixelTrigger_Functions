034f235f63a5d6db682c7373f09cd5c31764907a
MCPost <christopher_postzich@web.de>
2021-12-25 17:51:51 +0100

    changed matlab function name

---
+++ PixelTrigger_BackgroundColor.m
@@ -0,0 +1,102 @@
+function rgb_trig_vals = PixelTrigger_Colors(backgroundcolor, varargin)
+
+%% Get Possible Combinations of Green and Blue values corresponding to different triggers
+
+%' Inputs:
+% 
+% backgroundcolor       3 element vector specifing the RGB values (between
+%                       0 and 255) of the background color (e.g. [200,200,200]
+% show_visual           If set to true, a figure with all different colors
+%                       that lead to different trigger values. For each
+%                       plot the left color is the backgroundcolor and the
+%                       right is the trigger color
+%                       (Note that those are not all colors resulting in 
+%                       their corresponding triggers)
+%
+%' Outputs:
+% 
+% rgb_trig_vals         255 x 4 list of RGB colors (column 1-3) and their 
+%                       trigger values (column 4).
+%
+% C.Postzich, 14.Dec.2021
+
+
+% Validate Function Inputs
+validateattributes(backgroundcolor,{'numeric'},{'numel',3,'>=',0,'<',255,'nonnan'},'PixelTrigger_Colors','backgroundcolor')
+
+if(isempty(varargin))
+    show_visual = false;
+else
+    validateattributes(varargin{1},{'logical'},{'numel',1},'PixelTrigger_Colors','show_visual')
+    show_visual = varargin{1};
+end
+
+
+all_combinations = dec2bin(0:255);
+
+rgb_trig_vals = repmat([backgroundcolor(1),0,0],size(all_combinations,1),1);
+for i = 1:size(all_combinations,1)
+    temp_bin_g = '00000000';
+    temp_bin_g(1:2:8) = all_combinations(i,1:4);
+    temp_bin_b = '00000000';
+    temp_bin_b(1:2:8) = all_combinations(i,5:8);
+    temp_comb = zeros(size(all_combinations,1),3);
+    for j = 1:size(all_combinations,1)
+        temp_bin_g(2:2:8) = all_combinations(j,1:4);
+        temp_bin_b(2:2:8) = all_combinations(j,5:8);
+        temp_comb(j,1:2) = [bin2dec(temp_bin_g) bin2dec(temp_bin_b)];
+        temp_diffcol = [transpose(0:255), repmat(temp_comb(j,1:2),256,1)];
+        [temp_comb(j,3), temp_comb(j,4)] = min(1 - dot(temp_diffcol, repmat(backgroundcolor,256,1),2) ./ vecnorm(temp_diffcol,2,2) ./ vecnorm(repmat(backgroundcolor,256,1),2,2));
+    end
+    [~,idx] = min(temp_comb(:,3));
+    
+    rgb_trig_vals(i,1:3) = [temp_comb(idx,4)-1 temp_comb(idx,1:2)];
+    rgb_trig_vals(i,4) = gb2trigger(rgb_trig_vals(i,2:3));
+end
+rgb_trig_vals(rgb_trig_vals(:,1) == backgroundcolor(1) &...
+              rgb_trig_vals(:,2) == backgroundcolor(2) &...
+              rgb_trig_vals(:,3) == backgroundcolor(3),:) = [];
+
+% Plot Colors belonging to trigger values if visuals are wanted
+if(show_visual)
+    
+    figure('pos', [112  57  1318  714])
+    for i = 1:size(rgb_trig_vals,1)
+        ax = axes('Position',[0.02+(0.98/17)*mod((i-1),17) 0.94-(0.98/15)*floor((i-1)/17) 0.04 0.04],'xtick',[],'ytick',[],'visible','off');
+        patch([0 0.5 0.5 0],[0 0 1 1],backgroundcolor./256, 'tag',sprintf('Color (%d,%d,%d)',backgroundcolor(1), backgroundcolor(2), backgroundcolor(3)))
+        patch([0.5 1 1 0.5],[0 0 1 1],rgb_trig_vals(i,1:3)./256)
+        text('parent',ax,  'String',num2str(rgb_trig_vals(i,4)),   'position',[0.5 1.25], 'FontSize',8,  'HorizontalAlignment','center')
+    end
+    dcm = datacursormode;
+    dcm.SnapToDataVertex = 'off';
+    dcm.Enable = 'on';
+    dcm.UpdateFcn = @displayColor;
+    
+end
+
+end
+
+% Callback function for verifying the blue and green values match the
+% trigger value
+function trig_val = gb2trigger(bg)
+
+green_bin = fliplr(dec2bin(bg(1)));
+if(length(green_bin) == 1)
+    trigger_bin = '0';
+else
+    trigger_bin = fliplr(green_bin(2:2:end));
+end
+blue_bin = fliplr(dec2bin(bg(2)));
+if(length(blue_bin) == 1)
+    trigger_bin = [trigger_bin, '0000'];
+else
+    trigger_bin = [trigger_bin, repmat('0',1,4-length(blue_bin(2:2:end))), fliplr(blue_bin(2:2:end))];
+end
+trig_val = bin2dec(trigger_bin);
+
+end
+
+% Callback function for displaying color info in the figure
+function txt = displayColor(~,info)
+    txt = sprintf('RGB(%d,%d,%d)',info.Target.FaceColor(1)*256,info.Target.FaceColor(2)*256,info.Target.FaceColor(3)*256);
+end
